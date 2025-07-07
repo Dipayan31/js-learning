2040533b84a8f0d4f9484555d94402d95134f4c3
Dipayan31 <dipayanhazra22@gmail.com>
2025-07-07 07:05:25 +0000

    conversion of data types

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -1,2 +1,11 @@
-let score = 33
+let score = "33"
 console.log(typeof score);
+console.log(typeof(score));
+let ValueInNumber = Number(score)
+console.log(typeof ValueInNumber);
+let isLoggedIn = 1
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+console.log( booleanIsLoggedIn)
+let someNumber = 12
+let stringNumber = String(someNumber)
+console.log( typeof stringNumber);
